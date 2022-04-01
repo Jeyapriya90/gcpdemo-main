9da3e878cf9b30dafa995f4d0249debd6aa1cf46
Jeyapriya90 <92372923+Jeyapriya90@users.noreply.github.com>
2022-04-01 23:02:02 +0530

    Create variables.tf

---
+++ environments/dev/DB_Server/variables.tf
@@ -0,0 +1,24 @@
+variable "project-id"{
+type = string
+}
+variable "sqldb-inst-name"{
+type = string
+}
+variable "sqldb-region"{
+type = string
+}
+variable "sqldb-name"{
+type = string
+}
+variable "sqldbpass"{
+type = string
+}
+variable "gke-cluster-name"{
+type = string
+}
+variable "gke-region"{
+type = string
+}
+variable "gke-node-name"{
+type = string
+}
