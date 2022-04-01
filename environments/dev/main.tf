787cbff6376399773a01143d04b206ccf2152e19
Jeyapriya90 <92372923+Jeyapriya90@users.noreply.github.com>
2022-04-01 22:36:56 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,27 +1,3 @@
-variable "project-id"{
-type = string
-}
-variable "sqldb-inst-name"{
-type = string
-}
-variable "sqldb-region"{
-type = string
-}
-variable "sqldb-name"{
-type = string
-}
-variable "sqldbpass"{
-type = string
-}
-variable "gke-cluster-name"{
-type = string
-}
-variable "gke-region"{
-type = string
-}
-variable "gke-node-name"{
-type = string
-}
 
 module "AppServer" {
   source = "./App_Server"
