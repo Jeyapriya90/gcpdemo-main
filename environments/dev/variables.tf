9737ca923d0e648ad9a7ff270b25401e7208a980
Jeyapriya90 <92372923+Jeyapriya90@users.noreply.github.com>
2022-04-02 14:48:07 +0530

    Update variables.tf

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -13,12 +13,3 @@ type = string
 variable "sqldbpass"{
 type = string
 }
-variable "gke-cluster-name"{
-type = string
-}
-variable "gke-region"{
-type = string
-}
-variable "gke-node-name"{
-type = string
-}
