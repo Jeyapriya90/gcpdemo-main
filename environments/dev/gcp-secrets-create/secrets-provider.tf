66d7e78615486f1a5a95d7bd06dc9e2ba1eb11c0
Jeyapriya90 <92372923+Jeyapriya90@users.noreply.github.com>
2022-04-02 19:34:13 +0530

    Update and rename secrets-provider.tf to provider-main.tf

--- environments/dev/gcp-secrets-create/secrets-provider.tf
+++
@@ -1,8 +0,0 @@
-#########################
-## GCP Provider - Main ##
-#########################
-provider "google-beta" {
- credentials = file("./gcp-secrets-create/dev-project-345909-e4a91809a5da.json")
- project     = "dev-project-345909"
- region      = "asia-southeast1"
-}
