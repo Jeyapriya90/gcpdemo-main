c228ebdfec893a63d354437547d0750105f28ec7
Jeyapriya90 <92372923+Jeyapriya90@users.noreply.github.com>
2022-04-03 08:03:27 +0530

    Update db_provider.tf

--- environments/dev/DB_Server/db_provider.tf
+++ environments/dev/DB_Server/db_provider.tf
@@ -3,3 +3,9 @@ provider "google" {
  project     = "dev-project-345909"
  region      = "asia-southeast1"
 }
+
+provider "google-beta" {
+ credentials = file("./DB_Server/db_key.json")
+ project     = "dev-project-345909"
+ region      = "asia-southeast1"
+}
