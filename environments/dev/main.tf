a78861936c9865ad3fc3f75eae93bbf1f9608ca6
Jeyapriya90 <92372923+Jeyapriya90@users.noreply.github.com>
2022-04-03 08:01:22 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,8 +1,8 @@
-module "GCPSecrets" {
+/*module "GCPSecrets" {
   source = "./gcp-secrets-create"
-  }
+  }*/
   
-/*module "AppServer" {
+module "AppServer" {
   source = "./App_Server"
   sql_vpc_id = module.DBServer.sql_vpc_id
   uname = module.DBServer.uname
@@ -13,10 +13,10 @@ module "GCPSecrets" {
 
 module "DBServer" {
   source = "./DB_Server"
-  local-admin-password = module.GCPSecrets.local-admin-password
+  #local-admin-password = module.GCPSecrets.local-admin-password
   wp_vpc_id = module.AppServer.wp_vpc_id
   static_ip_wp = module.AppServer.static_ip_wp
-}*/
+}
 
 # output "name" {
 #   value = module.AppServer.ip
