459a64513ab018b4b8bd5dacf2596e526e6f27ef
Jeyapriya90 <92372923+Jeyapriya90@users.noreply.github.com>
2022-04-03 07:25:46 +0530

    Update secret-manager-auto-replication.tf

--- environments/dev/gcp-secrets-create/secret-manager-auto-replication.tf
+++ environments/dev/gcp-secrets-create/secret-manager-auto-replication.tf
@@ -6,22 +6,6 @@
 data "google_project" "prod_project" {
 }
 
-resource "google_secret_manager_secret" "local-admin-username" {
-  provider = google-beta
-
-  secret_id   = "local-admin-username"
-
-  replication {
-    automatic = true
-  }
-}
-
-# Add the secret data for local-admin-username secret
-resource "google_secret_manager_secret_version" "local-admin-username" {
-  secret = google_secret_manager_secret.local-admin-username.id
-  secret_data = "kopiadmin"
-}
-
 # Create a secret for local-admin-password
 resource "google_secret_manager_secret" "local-admin-password" {
   provider = google-beta
