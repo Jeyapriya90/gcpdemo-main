59cb3d51484af30195c7c47402f74e41c31bd9c3
Jeyapriya90 <92372923+Jeyapriya90@users.noreply.github.com>
2022-04-03 06:58:09 +0530

    Update secrets-provider.tf

--- environments/dev/gcp-secrets-create/secrets-provider.tf
+++ environments/dev/gcp-secrets-create/secrets-provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
- credentials = file("./gcp-secrets-create/db_sa.json")
+ credentials = file("./gcp-secrets-create/dev-sa.json")
  project     = "dev-project-345909"
  region      = "asia-southeast1"
 }
